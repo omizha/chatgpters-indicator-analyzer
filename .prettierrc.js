3068ae65553b22a11da82d99997e93d01b6d53f6
anpigon <markan82@email.com>
2023-06-03 10:44:01 +0900

    chore: add Prettier configuration file and VSCode settings for automatic formatting and linting
    The Prettier configuration file was added to the project to define code formatting rules. The VSCode settings were updated to enable automatic formatting on save using the Prettier formatter and to enable automatic linting using ESLint.

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  endOfLine: 'auto',
+  orderedImports: true,
+  parser: 'typescript',
+  printWidth: 120,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'all',
+  useTabs: false,
+};
