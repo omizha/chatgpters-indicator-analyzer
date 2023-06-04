c1394f4cc2943bfad6c6c834b3f97bf0738850e7
anpigon <anpigon@email.com>
2023-06-04 11:08:04 +0900

    chore(summarize.service.ts): add TODO comment about the issue with scrapping Naver blogs

--- app/server/src/summarize/summarize.service.ts
+++ app/server/src/summarize/summarize.service.ts
@@ -5,6 +5,7 @@ import { Document } from 'langchain/document';
 @Injectable()
 export class SummarizeService {
   async scrapper(url: string): Promise<Document[]> {
+    // TODO: 네이버 블로그를 스크래핑 못하는 문제가 있음
     const loader = new CheerioWebBaseLoader(url);
     const docs = await loader.load();
     return docs;
