1c2275ae1224ae7c90861a50d41436d3668bedaa
anpigon <markan82@gmail.com>
2023-06-11 11:45:15 +0900

    feat(next.config.js): add rewrite rule to redirect /api/* requests to http://127.0.0.1:3000/* to enable proxying of API requests to a backend server

--- app/client/next.config.js
+++ app/client/next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
     // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
     styledComponents: true,
   },
+  async rewrites() {
+    return [
+      {
+        destination: 'http://127.0.0.1:3000/:path*',
+        source: '/api/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
